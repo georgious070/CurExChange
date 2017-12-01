c0e1b52d6b7f3ccd01e5aecbe1f790779b0270eb
George <george070skv@gmail.com>
2017-12-01 19:32:26 +0200

    Create db via DbHelper

---
+++ app/src/main/java/com/examle/curexchange/data/database/DbHelper.java
@@ -0,0 +1,32 @@
+package com.examle.curexchange.data.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.examle.curexchange.data.database.CurrencyContract.CurrencyEntry;
+
+import java.util.Currency;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "currency.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_CURRENCY_TABLE = "CREATE TABLE " + CurrencyEntry.TABLE_NAME + "("
+                + CurrencyEntry.COLUMN_CODE + " TEXT PRIMARY KEY,"
+                + CurrencyEntry.COLUMN_CRYPTO_NAME + " TEXT);";
+        db.execSQL(SQL_CREATE_CURRENCY_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //no-op
+    }
+}
