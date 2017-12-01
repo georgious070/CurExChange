90fac373111797f159ccecb998895cf2481b41fa
George <george070skv@gmail.com>
2017-12-01 16:26:03 +0200

    Upgrade dagger

--- app/src/main/java/com/examle/curexchange/App.java
+++ app/src/main/java/com/examle/curexchange/App.java
@@ -2,8 +2,13 @@ package com.examle.curexchange;
 
 import android.app.Application;
 
+import com.examle.curexchange.di.component.AppComponent;
+import com.examle.curexchange.di.component.DaggerAppComponent;
+import com.examle.curexchange.di.module.AppModule;
+
 public class App extends Application {
 
+    private AppComponent appComponent;
     private static App app;
 
     public static App getApp() {
@@ -16,5 +21,10 @@ public class App extends Application {
         app = this;
     }
 
-
+    public AppComponent getAppComponent() {
+        appComponent = DaggerAppComponent.builder()
+                .appModule(new AppModule(this))
+                .build();
+        return appComponent;
+    }
 }
--- app/src/main/java/com/examle/curexchange/di/component/AppComponent.java
+++ app/src/main/java/com/examle/curexchange/di/component/AppComponent.java
@@ -1,5 +1,15 @@
 package com.examle.curexchange.di.component;
 
+import com.examle.curexchange.di.module.AppModule;
+import com.examle.curexchange.ui.home.CurrencyPresenter;
 
-public class AppComponent {
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+@Singleton
+@Component(modules = {AppModule.class})
+public interface AppComponent {
+
+    void inject(CurrencyPresenter currencyPresenter);
 }
