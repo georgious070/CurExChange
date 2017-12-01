f165603fc5b79ba704f2c57785650dc53a65b969
George <george070skv@gmail.com>
2017-12-01 19:26:11 +0200

    Create database contract.

---
+++ app/src/main/java/com/examle/curexchange/data/database/CurrencyContract.java
@@ -0,0 +1,21 @@
+package com.examle.curexchange.data.database;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+
+public class CurrencyContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.currency_db";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_CURRENCY = "currency";
+
+    public static class CurrencyEntry {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_CURRENCY);
+        public static final String TABLE_NAME = "crypto_codes";
+        public static final String COLUMN_CODE = "code";
+        public static final String COLUMN_CRYPTO_NAME = "crypto_name";
+        public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +
+                CONTENT_AUTHORITY + "/" + PATH_CURRENCY;
+    }
+}
