2c61bcef1c13f85bbea6ba2f5d19974821d47921
George <george070skv@gmail.com>
2017-12-01 15:34:15 +0200

    Create POJO model.

--- app/src/main/java/com/examle/curexchange/data/remote/CryptoCodeApi.java
+++ app/src/main/java/com/examle/curexchange/data/remote/CryptoCodeApi.java
@@ -1,5 +1,10 @@
 package com.examle.curexchange.data.remote;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface CryptoCodeApi {
 
+    @GET("currencies")
+    Call<String> getCryptoCodes();
 }
