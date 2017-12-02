b714e3800693b2dd51c68c787e4a746a990d7fd7
George <george070skv@gmail.com>
2017-12-03 00:17:01 +0200

    Add AsyncQueryHandler to query data in background thread.

--- app/src/main/java/com/examle/curexchange/data/repository/ExchangeRepository.java
+++ app/src/main/java/com/examle/curexchange/data/repository/ExchangeRepository.java
@@ -1,32 +1,58 @@
 package com.examle.curexchange.data.repository;
 
 
+import android.annotation.SuppressLint;
 import android.content.AsyncQueryHandler;
+import android.database.Cursor;
+import android.net.Uri;
 
+import com.examle.curexchange.App;
+import com.examle.curexchange.data.database.CurrencyContract;
 import com.examle.curexchange.ui.result.ExchangeCallback;
 
 import javax.inject.Inject;
 
 public class ExchangeRepository {
 
+    private static AsyncQueryHandler handler;
 
     @Inject
     public ExchangeRepository() {
     }
 
-    void loadExchangeValue(){
+    void loadExchangeValue() {
 
     }
 
-    void getResult(final ExchangeCallback exchangeCallback){
+    void getResult(final ExchangeCallback exchangeCallback, String name) {
         queryCodesFromDB(new QueryCodeCallback() {
             @Override
             public void onSuccess(String code) {
 
             }
-        });
+        }, name);
     }
 
-    void queryCodesFromDB(QueryCodeCallback queryCodeCallback){
+    @SuppressLint("HandlerLeak")
+    void queryCodesFromDB(QueryCodeCallback queryCodeCallback, String name) {
+
+        String[] projection = {CurrencyContract.CurrencyEntry.COLUMN_CODE};
+        String selection = CurrencyContract.CurrencyEntry.COLUMN_CRYPTO_NAME + " =?";
+        String[] selectionArgs = {name};
+
+        handler = new AsyncQueryHandler(App.getApp().getContentResolver()) {
+            @Override
+            protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
+                super.onQueryComplete(token, cookie, cursor);
+            }
+        };
+
+        handler.startQuery(1,
+                null,
+                CurrencyContract.CurrencyEntry.CONTENT_URI,
+                projection,
+                selection,
+                selectionArgs,
+                null);
     }
 }
