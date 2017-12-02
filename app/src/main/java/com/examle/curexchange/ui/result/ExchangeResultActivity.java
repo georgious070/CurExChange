ef6f65f9af816c1d518bef8993c78ee567ab6995
George <george070skv@gmail.com>
2017-12-02 20:23:08 +0200

    Add result activity to show result of exchanging selected before currencies.

---
+++ app/src/main/java/com/examle/curexchange/ui/result/ExchangeResultActivity.java
@@ -0,0 +1,16 @@
+package com.examle.curexchange.ui.result;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.examle.curexchange.R;
+
+public class ExchangeResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exchange_result);
+
+    }
+}
