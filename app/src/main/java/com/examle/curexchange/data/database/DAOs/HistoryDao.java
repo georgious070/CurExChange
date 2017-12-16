44891d1346d7f25282721c1a479b25727e213e69
George <george070skv@gmail.com>
2017-12-16 19:07:16 +0200

    Add insert method to HistoryDao to insert history of requests in HistoryTable.

--- app/src/main/java/com/examle/curexchange/data/database/DAOs/HistoryDao.java
+++ app/src/main/java/com/examle/curexchange/data/database/DAOs/HistoryDao.java
@@ -1,7 +1,13 @@
 package com.examle.curexchange.data.database.DAOs;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+
+import com.examle.curexchange.data.database.entities.HistoryEntity;
 
 @Dao
 public interface HistoryDao {
+
+    @Insert
+    void insertOneRaw(HistoryEntity historyEntity);
 }
