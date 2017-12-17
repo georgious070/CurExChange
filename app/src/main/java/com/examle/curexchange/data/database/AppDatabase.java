f6007410b9bbdc2096a95de9b91b9951a4f1cb40
George <george070skv@gmail.com>
2017-12-17 11:42:57 +0200

    Add to module db provide methods.

--- app/src/main/java/com/examle/curexchange/data/database/AppDatabase.java
+++ app/src/main/java/com/examle/curexchange/data/database/AppDatabase.java
@@ -4,11 +4,15 @@ import android.arch.persistence.room.Database;
 
 import android.arch.persistence.room.RoomDatabase;
 
+import com.examle.curexchange.data.database.DAOs.CurrencyDao;
+import com.examle.curexchange.data.database.DAOs.HistoryDao;
 import com.examle.curexchange.data.database.entities.CurrencyEntity;
 import com.examle.curexchange.data.database.entities.HistoryEntity;
 
 @Database(entities = {CurrencyEntity.class, HistoryEntity.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
+    public abstract CurrencyDao currencyDao();
 
+    public abstract HistoryDao historyDao();
 }
--- app/src/main/java/com/examle/curexchange/di/module/AppModule.java
+++ app/src/main/java/com/examle/curexchange/di/module/AppModule.java
@@ -1,9 +1,13 @@
 package com.examle.curexchange.di.module;
 
+import android.arch.persistence.room.Room;
 import android.content.Context;
 
 import com.examle.curexchange.App;
 import com.examle.curexchange.Constants;
+import com.examle.curexchange.data.database.AppDatabase;
+import com.examle.curexchange.data.database.DAOs.CurrencyDao;
+import com.examle.curexchange.data.database.DAOs.HistoryDao;
 import com.examle.curexchange.data.remote.ApiCryptoCode;
 import com.examle.curexchange.data.remote.ApiExchange;
 
@@ -60,5 +64,25 @@ public class AppModule {
     @Singleton
     ApiExchange provideExchangeApi(@Named("exchange") Retrofit retrofit) {
         return retrofit.create(ApiExchange.class);
+    }
+
+    @Provides
+    @Singleton
+    AppDatabase provideAppDatabase(App app) {
+        return Room
+                .databaseBuilder(app, AppDatabase.class, Constants.DATABASE_NAME)
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    CurrencyDao provideCurrencyDao(AppDatabase appDatabase) {
+        return appDatabase.currencyDao();
+    }
+
+    @Provides
+    @Singleton
+    HistoryDao provideHistoryDao(AppDatabase appDatabase) {
+        return appDatabase.historyDao();
     }
 }
