6a212c823d23bbbc97a1a58cd6b0c301de92737a
George <george070skv@gmail.com>
2017-12-23 12:59:17 +0200

    Refactoring

--- app/src/main/java/com/examle/curexchange/data/repository/currency/CurrencyRepository.java
+++ app/src/main/java/com/examle/curexchange/data/repository/currency/CurrencyRepository.java
@@ -1,8 +1,5 @@
 package com.examle.curexchange.data.repository.currency;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteException;
-
 import com.examle.curexchange.data.database.dao.CurrencyDao;
 import com.examle.curexchange.data.database.entities.CurrencyEntity;
 import com.examle.curexchange.data.model.pojo.CryptoCode;
@@ -38,46 +35,43 @@ public class CurrencyRepository {
     }
 
     public Flowable<List<String>> getNames() {
-        Flowable<List<String>> ob = Flowable.concat(insertToDbFromNetworkCall(), queryData())
-                .first()
+        return Flowable.merge(insertToDbFromNetworkCall(), queryData())
                 .subscribeOn(Schedulers.io());
-        return ob;
     }
 
     private Flowable<List<String>> insertToDbFromNetworkCall() {
         return observableCryptoCode
                 .subscribeOn(Schedulers.io())
-                .concatMap(cryptoCode -> {
+                .doOnNext(cryptoCode -> {
                     rows.addAll(cryptoCode.getRows());
                     for (int i = 0; i < rows.size(); i++) {
                         currencyEntities.add(new CurrencyEntity(rows.get(i).getCode(), rows.get(i).getName()));
                     }
                     currencyDao.insertAll(currencyEntities);
-                    return Flowable.just(names);
-                }).observeOn(Schedulers.computation());
+                })
+                .concatMap(cryptoCode -> Flowable.just(names))
+                .observeOn(Schedulers.computation());
     }
 
     private Flowable<List<String>> queryData() {
-        Flowable<List<String>> flowable = observableQuery
+        return observableQuery
                 .doOnNext(entityNames -> names.addAll(entityNames))
-                .filter(strings -> !strings.isEmpty())
                 .subscribeOn(Schedulers.io())
                 .observeOn(Schedulers.computation());
-        return flowable;
     }
-
-    private boolean isDbEmpty() {
-        try {
-            Cursor c = currencyDao.queryOneLine();
-            if (c.moveToFirst()) {
-                c.close();
-                return false;
-            } else {
-                c.close();
-                return true;
-            }
-        } catch (SQLiteException e) {
-            return true;
-        }
-    }
+//
+//    private boolean isDbEmpty() {
+//        try {
+//            Cursor c = currencyDao.queryOneLine();
+//            if (c.moveToFirst()) {
+//                c.close();
+//                return false;
+//            } else {
+//                c.close();
+//                return true;
+//            }
+//        } catch (SQLiteException e) {
+//            return true;
+//        }
+//    }
 }
