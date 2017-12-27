6bb23b7931db7924caa227a0f846a6cdcc7fd238
George <george070skv@gmail.com>
2017-12-28 00:10:02 +0200

    Change operator to flatMap to work with different data types.

--- app/src/main/java/com/examle/curexchange/data/repository/currency/CurrencyRepository.java
+++ app/src/main/java/com/examle/curexchange/data/repository/currency/CurrencyRepository.java
@@ -1,18 +1,31 @@
 package com.examle.curexchange.data.repository.currency;
 
+import android.support.annotation.NonNull;
+
 import com.examle.curexchange.data.database.dao.CurrencyDao;
 import com.examle.curexchange.data.database.entities.CurrencyEntity;
 import com.examle.curexchange.data.model.pojo.CryptoCode;
 import com.examle.curexchange.data.model.pojo.Row;
 import com.examle.curexchange.data.remote.ApiCryptoCode;
 
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Observable;
+import java.util.concurrent.Callable;
 
 import javax.inject.Inject;
 
 import io.reactivex.Flowable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subscribers.DisposableSubscriber;
 
 public class CurrencyRepository {
 
@@ -31,32 +44,35 @@ public class CurrencyRepository {
         this.currencyDao = currencyDao;
         this.observableCryptoCode = apiCryptoCode.getCryptoCodes();
         this.observableQuery = currencyDao.queryCryptoNames();
-
     }
 
     public Flowable<List<String>> getNames() {
-        return insertToDbFromNetworkCall().concatWith(queryData())
-                .subscribeOn(Schedulers.io());
-    }
-
-    private Flowable<List<String>> insertToDbFromNetworkCall() {
-        return observableCryptoCode
-                .subscribeOn(Schedulers.io())
-                .doOnNext(cryptoCode -> {
-                    rows.addAll(cryptoCode.getRows());
-                    for (int i = 0; i < rows.size(); i++) {
-                        currencyEntities.add(new CurrencyEntity(rows.get(i).getCode(), rows.get(i).getName()));
-                    }
-                    currencyDao.insertAll(currencyEntities);
-                })
-                .concatMap(cryptoCode -> Flowable.just(names))
-                .observeOn(Schedulers.computation());
+        return queryData();
     }
 
     private Flowable<List<String>> queryData() {
-        return observableQuery
-                .doOnNext(entityNames -> names.addAll(entityNames))
+        return observableCryptoCode
                 .subscribeOn(Schedulers.io())
-                .observeOn(Schedulers.computation());
+                .flatMap(new Function<CryptoCode, Publisher<List<String>>>() {
+                    @Override
+                    public Publisher<List<String>> apply(CryptoCode code) throws Exception {
+                        if(false){
+                            insertToDbFromNetwork(code);
+                        }
+                        return observableQuery
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .doOnNext(entityNames -> names.addAll(entityNames));
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    private void insertToDbFromNetwork(CryptoCode code){
+        rows.addAll(code.getRows());
+        for (int i = 0; i < rows.size(); i++) {
+            currencyEntities.add(new CurrencyEntity(rows.get(i).getCode(), rows.get(i).getName()));
+        }
+        currencyDao.insertAll(currencyEntities);
     }
 }
