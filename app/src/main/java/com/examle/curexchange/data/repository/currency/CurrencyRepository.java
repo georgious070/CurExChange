80997559dbacec9f3609887501b507d0a2c6a69f
George <george070skv@gmail.com>
2017-12-28 00:15:04 +0200

    Again add checking if db empty

--- app/src/main/java/com/examle/curexchange/data/repository/currency/CurrencyRepository.java
+++ app/src/main/java/com/examle/curexchange/data/repository/currency/CurrencyRepository.java
@@ -1,6 +1,7 @@
 package com.examle.curexchange.data.repository.currency;
 
-import android.support.annotation.NonNull;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteException;
 
 import com.examle.curexchange.data.database.dao.CurrencyDao;
 import com.examle.curexchange.data.database.entities.CurrencyEntity;
@@ -8,24 +9,14 @@ import com.examle.curexchange.data.model.pojo.CryptoCode;
 import com.examle.curexchange.data.model.pojo.Row;
 import com.examle.curexchange.data.remote.ApiCryptoCode;
 
-import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Observable;
-import java.util.concurrent.Callable;
 
 import javax.inject.Inject;
 
 import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subscribers.DisposableSubscriber;
 
 public class CurrencyRepository {
 
@@ -53,26 +44,38 @@ public class CurrencyRepository {
     private Flowable<List<String>> queryData() {
         return observableCryptoCode
                 .subscribeOn(Schedulers.io())
-                .flatMap(new Function<CryptoCode, Publisher<List<String>>>() {
-                    @Override
-                    public Publisher<List<String>> apply(CryptoCode code) throws Exception {
-                        if(false){
-                            insertToDbFromNetwork(code);
-                        }
-                        return observableQuery
-                                .subscribeOn(Schedulers.io())
-                                .observeOn(AndroidSchedulers.mainThread())
-                                .doOnNext(entityNames -> names.addAll(entityNames));
+                .flatMap(code -> {
+                    if (isDbEmpty()) {
+                        insertToDbFromNetwork(code);
                     }
+                    return observableQuery
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .doOnNext(entityNames -> names.addAll(entityNames));
                 })
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-    private void insertToDbFromNetwork(CryptoCode code){
+    private void insertToDbFromNetwork(CryptoCode code) {
         rows.addAll(code.getRows());
         for (int i = 0; i < rows.size(); i++) {
             currencyEntities.add(new CurrencyEntity(rows.get(i).getCode(), rows.get(i).getName()));
         }
         currencyDao.insertAll(currencyEntities);
+    }
+
+    private boolean isDbEmpty() {
+        try {
+            Cursor c = currencyDao.queryOneLine();
+            if (c.moveToFirst()) {
+                c.close();
+                return false;
+            } else {
+                c.close();
+                return true;
+            }
+        } catch (SQLiteException e) {
+            return true;
+        }
     }
 }
