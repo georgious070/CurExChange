9c6ca77ec753b9fbd3f4ecf1429102bae1b764a6
George <george070skv@gmail.com>
2018-01-21 14:01:05 +0200

    Base from java to kotlin

---
+++ app/src/main/java/com/examle/curexchange/ui/base/BaseView.kt
@@ -0,0 +1,11 @@
+package com.examle.curexchange.ui.base
+
+import com.arellomobile.mvp.MvpView
+
+
+interface BaseView : MvpView{
+
+    fun showToast(error: String)
+
+    fun showProgress(isShow: Boolean)
+}
