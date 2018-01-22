686eca46de7fa877efae64e724167d231ecd6b75
George <george070skv@gmail.com>
2018-01-22 23:03:05 +0200

    Utils classes to kotlin

---
+++ app/src/main/java/com/examle/curexchange/Constants.kt
@@ -0,0 +1,12 @@
+package com.examle.curexchange
+
+object Constants {
+    const val DATABASE_NAME = "currency.db"
+    const val DATABASE_VERSION = 2
+    const val CRYPTO_CODE_URL = "https://api.cryptonator.com/api/"
+    const val EXCHANGE_URL = "https://api.cryptonator.com/api/"
+    const val INTENT_KEY_NAMES = "names"
+    const val INTENT_KEY_FIRST_NAME = "firstName"
+    const val INTENT_KEY_SECOND_NAME = "secondName"
+    const val INTENT_KEY_VALUE = "value"
+}
--- app/src/main/java/com/examle/curexchange/data/database/AppDatabase.java
+++ app/src/main/java/com/examle/curexchange/data/database/AppDatabase.java
@@ -4,14 +4,15 @@ import android.arch.persistence.room.Database;
 
 import android.arch.persistence.room.RoomDatabase;
 
+import com.examle.curexchange.Constants;
 import com.examle.curexchange.data.database.dao.CurrencyDao;
 import com.examle.curexchange.data.database.dao.HistoryDao;
 import com.examle.curexchange.data.database.entities.CurrencyEntity;
 import com.examle.curexchange.data.database.entities.HistoryEntity;
 
-import static com.examle.curexchange.Constants.DATABASE_VERSION;
-
-@Database(entities = {CurrencyEntity.class, HistoryEntity.class}, version = DATABASE_VERSION, exportSchema = false)
+@Database(entities = {CurrencyEntity.class, HistoryEntity.class},
+        version = Constants.DATABASE_VERSION,
+        exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract CurrencyDao currencyDao();
